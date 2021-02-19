f8f288049cc9aaf8743fcce5c17e232d214645bb
Aimee Rawlings <aimeeclairerawlings@gmail.com>
2021-02-19 14:26:45 +0200

    Changed views and added review model

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -26,7 +26,7 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :cocktails, only: [ :new, :create, :index, :show ] do
-    resources :doses, only: [ :new, :create ]
+  root to: 'cocktails#index'
+  resources :cocktails, only: [ :new, :create, :index, :show ] do
+  resources :doses, only: [ :new, :create ]
+  resources :reviews, only: [:new, :create]
   end
     resources :doses, only: [ :destroy ]
 end
